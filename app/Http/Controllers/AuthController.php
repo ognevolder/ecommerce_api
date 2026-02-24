638ecc2afbe8b59a8c40123135e2415ef800c2f2
ognevolder <ognevolder.webdev@gmail.com>
2026-02-24 11:43:30 +0100

    Sanctum API Auth-system

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegistrationRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(RegistrationRequest $request)
+    {
+        //Validate data
+        $attributes = $request->validated();
+        //Create user
+        $user = User::create($attributes);
+        //Return token
+        $user->tokens()->delete();
+        $token = $user->createToken('auth_token')->plainTextToken;
+        //Response
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        //Validation
+        $credentials = $request->validated();
+        $status = Auth::attempt($credentials);
+        //Login
+        if (! $status)
+            {
+                return response()->json([
+                    'message' => 'Error is occured.',
+                    'status' => '401'
+                ]);
+            }
+
+        $user = User::where('email', $request->email)->first();
+        $user->tokens()->delete();
+        $token = $user->createToken('auth_token', ['*'], Carbon::now()->addMinutes(180))->plainTextToken;
+        //Response/redirect
+        return response()->json([
+            'message' => 'Logged In',
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json([
+            'message' => 'Logged Out'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,19 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/api/test', function ()
+Route::get('/test', function ()
   {
     return response()->json([
       'message' => 'API works.',
       'status' => '200'
     ]);
   });
+
+//Public
+Route::post('/registration', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+//Auth
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
